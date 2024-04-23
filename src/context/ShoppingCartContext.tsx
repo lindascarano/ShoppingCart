e46b0ad96f74b94a3d2f5d540c0f734cd3eea3c0
Linda Scarano <lindascarano@gmail.com>
2024-04-23 20:05:42 +0200

    Feat: Create ShoppingCartContext

---
+++ src/context/ShoppingCartContext.tsx
@@ -0,0 +1,26 @@
+import { ReactNode, createContext, useContext } from "react";
+
+type ShoppingCartProviderProps = {
+  children: ReactNode;
+};
+
+type ShoppingCartContext = {
+  getItemQuantity: (id: number) => number;
+  increaseItemQuantity: (id: number) => void;
+  decreaseItemQuantity: (id: number) => void;
+  removeItemQuantity: (id: number) => void;
+};
+
+const ShoppingCartContext = createContext({});
+
+export function useShoppingCart() {
+  return useContext(ShoppingCartContext);
+}
+
+export function ShoppingCartProvider({ children }: ShoppingCartProviderProps) {
+  return (
+    <ShoppingCartContext.Provider value={{}}>
+      {children}
+    </ShoppingCartContext.Provider>
+  );
+}
