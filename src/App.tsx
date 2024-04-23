c483e024bd6bcd3d85324bf76a8192502039af2a
Linda Scarano <lindascarano@gmail.com>
2024-04-23 18:03:26 +0200

    Feat: Create Component Navbar.tsx with Routes and Cart Icon

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,12 @@ import { Route, Routes } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { Store } from "./pages/Store";
 import { About } from "./pages/About";
+import { Navbar } from "./components/Navbar";
 
 function App() {
   return (
+    <>
+    <Navbar/>
     <Container className="mb-4">
       <Routes>
         <Route path="/" element={<Home />} />
@@ -13,6 +16,8 @@ function App() {
         <Route path="/about" element={<About />} />
       </Routes>
     </Container>
+    </>
+    
   );
 }
 
